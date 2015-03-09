aa66138a445897536643aa6c1b710716ec230c88
Linda Swanson <linda.swanson@detroitlabs.com>
2015-03-09 11:59:48 -0400

    Assessment 4: Zookeeper

---
+++ assessment4.js
@@ -0,0 +1,10 @@
+/* Author: Linda Swanson
+Functions
+
+Allow user to take care of an animal in the zoo. They will be able to:
+- Name the animal
+- Feed it
+- Let it out of its cage
+- Clean the cage
+- Wrangle the animal back to its cage
+*/
